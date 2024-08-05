5dd11116061fcef0896c83d466c1d44012d92b56
eliharriet7p <eliana.harriet@7puentes.com>
2024-08-05 16:41:14 -0300

    feat: Add psycopg2-binary and SQLAlchemy to requirements.txt

---
+++ lib/utils.py
@@ -0,0 +1,19 @@
+import os
+from typing import Optional, Union
+from sqlalchemy import create_engine
+
+HOUR = 60 * 60
+MINUTE = 60
+MILLISECOND = 1_000
+
+def time_to_string(time_in_seconds: float) -> str:
+    minutes = int(time_in_seconds // MINUTE)
+    seconds = int(time_in_seconds % MINUTE)
+    millis = int((time_in_seconds - int(time_in_seconds)) * MILLISECOND)
+    return f"{minutes}m {seconds}s {millis}ms"
+
+def get_engine() -> Optional[create_engine]:
+    return create_engine(
+        f"postgresql+psycopg2://{os.getenv('POSTGRES_USER')}:{os.getenv('POSTGRES_PASSWORD')}@{os.getenv('POSTGRES_HOST')}:{os.getenv('POSTGRES_PORT')}/{os.getenv('POSTGRES_DB')}",
+        echo=True
+    )
