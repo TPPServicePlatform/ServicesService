da8bf5cdef4f1bc9c175494afc6bea674bc760dd
Eliana Harriet <eharriet@fi.uba.ar>
2024-09-09 15:49:45 -0300

    Refactor Services class to use MongoDB instead of SQL

--- api_container/services_api.py
+++ api_container/services_api.py
@@ -1,4 +1,6 @@
+from typing import Optional
 from services_nosql import Services
+from ratings_nosql import Ratings
 import mongomock
 import logging as logger
 import time
@@ -43,9 +45,11 @@ app.add_middleware(
 
 if os.getenv('TESTING'):
     client = mongomock.MongoClient()
-    sql_manager = Services(test_client=client)
+    services_manager = Services(test_client=client)
+    ratings_manager = Ratings(test_client=client)
 else:
-    sql_manager = Services()
+    services_manager = Services()
+    ratings_manager = Ratings()
 
 REQUIRED_CREATE_FIELDS = {"service_name", "provider_username", "category", "price"}
 OPTIONAL_CREATE_FIELDS = {"description"}
@@ -58,7 +62,7 @@ logger.info(f"Services API started in {starting_duration}")
 
 @app.get("/{uuid}")
 def get(uuid: str):
-    services = sql_manager.get(uuid)
+    services = services_manager.get(uuid)
     if not services:
         raise HTTPException(status_code=404, detail=f"Service with uuid '{uuid}' not found")
     return services
@@ -73,14 +77,14 @@ def create(body: dict):
     
     data.update({field: None for field in OPTIONAL_CREATE_FIELDS if field not in data})
 
-    uuid = sql_manager.insert(data["service_name"], data["provider_username"], data["description"], data["category"], data["price"])
+    uuid = services_manager.insert(data["service_name"], data["provider_username"], data["description"], data["category"], data["price"])
     if not uuid:
         raise HTTPException(status_code=400, detail="Error creating service")
     return {"status": "ok", "service_id": uuid}
 
 @app.delete("/{id}")
 def delete(id: str):
-    if not sql_manager.delete(id):
+    if not services_manager.delete(id):
         raise HTTPException(status_code=404, detail="Service not found")
     return {"status": "ok"}
 
@@ -94,9 +98,42 @@ def update(id: str, body: dict):
     if not_valid_fields:
         raise HTTPException(status_code=400, detail=f"Invalid fields: {', '.join(not_valid_fields)}")
     
-    if not sql_manager.get(id):
+    if not services_manager.get(id):
         raise HTTPException(status_code=404, detail="Service not found")
     
-    if not sql_manager.update(id, update):
+    if not services_manager.update(id, update):
         raise HTTPException(status_code=400, detail="Error updating service")
     return {"status": "ok"}
+
+@app.put("/{id}/reviews")
+def review(id: str, rating: int, comment: Optional[str], user_uuid: str):
+    if not services_manager.get(id):
+        raise HTTPException(status_code=404, detail="Service not found")
+    
+    older_review_uuid = ratings_manager.get_review(id, user_uuid).get("uuid", None)
+    if older_review_uuid is not None:
+        if not ratings_manager.update(older_review_uuid, rating, comment):
+            raise HTTPException(status_code=400, detail="Error updating review")
+        return {"status": "ok", "review_id": older_review_uuid}
+    
+    review_uuid = ratings_manager.insert_review(id, rating, comment, user_uuid)
+    if not review_uuid:
+        raise HTTPException(status_code=400, detail="Error creating review")
+    return {"status": "ok", "review_id": review_uuid}
+
+@app.delete("/{id}/reviews")
+def delete_review(id: str, user_uuid: str):
+    review = ratings_manager.get_review(id, user_uuid)
+    if not review:
+        raise HTTPException(status_code=404, detail="Review not found")
+    
+    if not ratings_manager.delete_review(review["uuid"]):
+        raise HTTPException(status_code=400, detail="Error deleting review")
+    return {"status": "ok"}
+
+@app.get("/{id}/reviews")
+def get_reviews(id: str):
+    reviews = ratings_manager.get_reviews(id)
+    if not reviews:
+        raise HTTPException(status_code=404, detail="Reviews not found")
+    return {"status": "ok", "reviews": reviews}
