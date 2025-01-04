93eb9012c700a42356f315c3fff2ab07774a2016
ElianaHarriet <eharriet@fi.uba.ar>
2025-01-04 00:02:36 -0300

    Implement Ratings class for MongoDB integration; add methods for retrieving recent ratings

--- export_lib/lib/utils.py
+++ export_lib/lib/utils.py
@@ -2,9 +2,16 @@ import os
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
 import logging as logger
+import datetime
+import time
+
+DAY = 24 * 60 * 60
 
 def get_mongo_client() -> MongoClient:
     uri = f"mongodb+srv://{os.getenv('MONGO_USER')}:{os.getenv('MONGO_PASSWORD')}@{os.getenv('MONGO_HOST')}/?retryWrites=true&w=majority&appName={os.getenv('MONGO_APP_NAME')}"
     print(f"Connecting to MongoDB: {uri}")
     logger.getLogger('pymongo').setLevel(logger.WARNING)
     return MongoClient(uri, server_api=ServerApi('1'))
+
+def get_time_past_days(days: int) -> str:
+    return datetime.datetime.fromtimestamp(time.time() - days * DAY).strftime('%Y-%m-%d %H:%M:%S')
--- export_lib/services_lib.py
+++ export_lib/services_lib.py
@@ -1,11 +1,13 @@
 from typing import Optional
 from imported_lib.ServicesService.lib.exportable_rentals_nosql import Rentals
 from imported_lib.ServicesService.lib.exportable_services_nosql import Services
+from imported_lib.ServicesService.lib.exportable_ratings_nosql import Ratings
 
 class ServicesLib:
     def __init__(self, test_client=None):
         self.rentals = Rentals(test_client)
         self.services = Services(test_client)
+        self.ratings = Ratings(test_client)
 
     def total_rentals(self, provider_id: str) -> int:
         return self.rentals.total_rentals(provider_id)
@@ -21,3 +23,9 @@ class ServicesLib:
         sum_rating = sum(rating['sum_rating'] for rating in ratings)
         num_ratings = sum(rating['num_ratings'] for rating in ratings)
         return {'avg_rating': sum_rating / num_ratings, 'num_ratings': num_ratings}
+    
+    def get_recent_ratings(self, max_delta_days: int) -> Optional[list[tuple[str, str, float]]]:
+        results = self.ratings.get_recent(max_delta_days)
+        if not results:
+            return None
+        return [(f"U{r['user_uuid']}", f"S{r['service_uuid']}", float(r['rating'])) for r in results]      
