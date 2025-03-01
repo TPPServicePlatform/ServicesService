ba37d45fdba8eb49bf9b8ff923b8d4e4ca98ae2f
ElianaHarriet <eharriet@fi.uba.ar>
2025-03-01 11:52:46 -0300

    Update MongoDB connection URI to use SRV format for improved connection handling

--- lib/utils.py
+++ lib/utils.py
@@ -22,7 +22,7 @@ def time_to_string(time_in_seconds: float) -> str:
 def get_mongo_client() -> MongoClient:
     if not all([os.getenv('MONGO_USER'), os.getenv('MONGO_PASSWORD'), os.getenv('MONGO_HOST'), os.getenv('MONGO_APP_NAME')]):
         raise HTTPException(status_code=500, detail="MongoDB environment variables are not set properly")
-    uri = f"mongodb://{os.getenv('MONGO_USER')}:{os.getenv('MONGO_PASSWORD')}@{os.getenv('MONGO_HOST')}/?retryWrites=true&w=majority&appName={os.getenv('MONGO_APP_NAME')}"
+    uri = f"mongodb+srv://{os.getenv('MONGO_USER')}:{os.getenv('MONGO_PASSWORD')}@{os.getenv('MONGO_HOST')}/?retryWrites=true&w=majority&appName={os.getenv('MONGO_APP_NAME')}"
     print(f"Connecting to MongoDB: {uri}")
     logger.getLogger('pymongo').setLevel(logger.WARNING)
     return MongoClient(uri)
