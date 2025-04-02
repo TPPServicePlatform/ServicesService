b6d2025056c08161b0274cdbe4d3052f021e3adc
ElianaHarriet <eharriet@fi.uba.ar>
2025-04-02 20:45:24 -0300

    Enhance interest prediction by connecting users in the data graph for improved recommendations

--- lib/interest_prediction.py
+++ lib/interest_prediction.py
@@ -13,6 +13,7 @@ class InterestPredictor:
         self.user_id = user_id
         # self.existing_services = {service for (user, service) in reviews if user == user_id}
         self._ebunch = self._get_ebunch(self.bipartite_graph, self.user_id)
+        self.data_graph = self._connect_users(reviews)
 
     def _create_bipartite_graph(self, reviews: List[Tuple[str, str]]) -> nx.Graph:
         bipartite_graph = nx.Graph()
@@ -22,8 +23,20 @@ class InterestPredictor:
     def _get_ebunch(self, graph: nx.Graph, user_id: str) -> List[Tuple[str, str]]:
         return [(user_id, service) for service in self.services if not graph.has_edge(user_id, service) and service in self.bipartite_graph]
     
+    def _connect_users(self, reviews: List[Tuple[str, str]]) -> nx.Graph:
+        data_graph = self.bipartite_graph.copy()
+        all_users = set(r[CLIENT_INDEX] for r in reviews)
+        for user in all_users:
+            for service in self.bipartite_graph.neighbors(user):
+                for other_user in self.bipartite_graph.neighbors(service):
+                    if user == other_user:
+                        continue
+                    if not data_graph.has_edge(user, other_user):
+                        data_graph.add_edge(user, other_user)
+        return data_graph
+                        
     def get_interest_prediction(self) -> List[str]:
-        predictions = nx.common_neighbor_centrality(self.bipartite_graph, ebunch=self._ebunch)
+        predictions = nx.common_neighbor_centrality(self.data_graph, ebunch=self._ebunch)
         return {service: score for (user, service, score) in predictions}
 
     
